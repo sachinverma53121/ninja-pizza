44dbbb00c665393025060e0af5f47771800c56be
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2019-11-16 17:48:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php 
+	include('config/db_connect.php');
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+	// make query and get result
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	//print_r($pizzas);
+
+	// free result form memory
+	mysqli_free_result($result);
+
+	//closing connection
+	mysqli_close($conn);
+
+	//explode(',', $pizzas[0]['ingredients']);
+
+
+
+ ?>
+
+<!DOCTYPE html>
+<html> 
+  <?php include('templates/header.php'); ?>
+
+  <h4 class="center grey-text">Pizzas!</h4>
+  <div class="container">
+  	<div class="row">
+  		<?php foreach($pizzas as $pizza) : ?>
+  			<div class="col s6 md3">
+  				<div class="card z-depth-0">
+  					<img src="img/pizza.svg" class="pizza">
+  					<div class="card-content center">
+  						<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+  						<ul>
+  							<?php foreach(explode(',', $pizza['ingredients']) as $ing) : ?>
+  								  <li><?php echo htmlspecialchars($ing); ?></li>
+  							<?php endforeach; ?>
+  						</ul>
+  					</div>
+  					<div class="card-action right-align">
+  						<a class="brand-text" href="details.php?id=<?php echo $pizza['id'] ?>">more info</a>
+  					</div>
+  				</div>
+  			</div>
+  		<?php endforeach; ?>
+  		<?php if(count($pizzas) >= 2) : ?>
+  			<p>there are 2 or more pizzas</p>
+  		<?php else : ?>
+  			<p>there are less than 2 pizzas</p>
+  		<?php endif; ?>	
+  	</div>
+  </div>	
+
+  <?php include('templates/footer.php'); ?>
+</html>
+
